3d9c91a18cf17624df793cb4939e9abc1e1a429d
amgithshaji <amgithshaji410@gmail.com>
2025-11-29 00:48:35 +0530

    project completed

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,8 +3,8 @@ import TiltedCard from './TiltedCard';
 
 function Cards() {
   return (
-    <div style={{ paddingLeft: "6rem",paddingRight: "6rem",marginBottom:"100px"}}  className='row g-1 d-flex justify-content-center g-5 align-items-center ' >
-<div style={{marginBottom:"80px"}} className='col-12 col-md-3  d-flex justify-content-center'>
+    <div style={{ paddingLeft: "6rem",paddingRight: "6rem",marginBottom:"100px"}}  className='row  d-flex justify-content-center g-5 align-items-center ' >
+<div style={{marginBottom:"80px",marginTop:"80px"}} className='col-12 col-md-3  d-flex justify-content-center'>
   <TiltedCard
     imageSrc="card1.jpg"
     altText=""
@@ -25,7 +25,7 @@ function Cards() {
     }
   />
 </div>
-<div style={{marginBottom:"80px"}}   className='col-12 col-md-3  d-flex justify-content-center'>
+<div style={{marginBottom:"80px",marginTop:"80px"}}   className='col-12 col-md-3  d-flex justify-content-center'>
   <TiltedCard
     imageSrc="card2.jpg"
     altText="Kendrick Lamar - GNX Album Cover"
@@ -46,7 +46,7 @@ function Cards() {
     }
   />
 </div>
-<div style={{marginBottom:"80px"}}  className='col-12 col-md-3 d-flex justify-content-center'>
+<div style={{marginBottom:"80px",marginTop:"80px"}}  className='col-12 col-md-3 d-flex justify-content-center'>
   <TiltedCard
     imageSrc="/card3.jpg"
     altText="Kendrick Lamar - GNX Album Cover"
@@ -67,7 +67,7 @@ function Cards() {
     }
   />
 </div>
-<div style={{marginBottom:"80px"}}  className='col-12 col-md-3  d-flex justify-content-center'>
+<div style={{marginBottom:"80px",marginTop:"80px"}}  className='col-12 col-md-3  d-flex justify-content-center'>
   <TiltedCard
     imageSrc="card4.jpg"
     altText="Kendrick Lamar - GNX Album Cover"
